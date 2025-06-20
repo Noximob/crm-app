bb05693f4bcf583a14b492b6a14d186227698d4b
NoxImoveis <imoveisnox@gmail.com>
2025-06-20 19:14:46 -0300

    fix(auth): Adiciona verificação de contexto no useAuth

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -54,4 +54,10 @@ export const AuthProvider = ({ children }: { children: ReactNode }) => {
   );
 };
 
-export const useAuth = () => useContext(AuthContext); 
+export const useAuth = () => {
+  const context = useContext(AuthContext);
+  if (context === undefined) {
+    throw new Error('useAuth must be used within an AuthProvider');
+  }
+  return context;
+}; 
